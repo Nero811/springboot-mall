aeaa3f8286dc082fa785c88e2247b61ad575139a
Nero811 <jy811005@gmail.com>
2023-07-20 22:45:33 +0800

    「商品功能 - 實作新增商品」

--- src/main/java/com/jackyang/springbootmall/dao/ProductDao.java
+++ src/main/java/com/jackyang/springbootmall/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.jackyang.springbootmall.dao;
 
+import com.jackyang.springbootmall.dto.ProductRequest;
 import com.jackyang.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/jackyang/springbootmall/service/ProductService.java
+++ src/main/java/com/jackyang/springbootmall/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.jackyang.springbootmall.service;
 
+import com.jackyang.springbootmall.dto.ProductRequest;
 import com.jackyang.springbootmall.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
