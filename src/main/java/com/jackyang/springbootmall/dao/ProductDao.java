0b9e793e8e113c6d4b59e1b279b95e4e7adfff02
Nero811 <jy811005@gmail.com>
2023-07-21 22:10:42 +0800

    「商品功能 - 實作查詢商品列表(查詢條件)」

--- src/main/java/com/jackyang/springbootmall/dao/ProductDao.java
+++ src/main/java/com/jackyang/springbootmall/dao/ProductDao.java
@@ -1,5 +1,6 @@
 package com.jackyang.springbootmall.dao;
 
+import com.jackyang.springbootmall.constant.ProductCategory;
 import com.jackyang.springbootmall.dto.ProductRequest;
 import com.jackyang.springbootmall.model.Product;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 public interface ProductDao {
 
-    List<Product> getProducts();
+    List<Product> getProducts(ProductCategory category, String search);
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/jackyang/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/jackyang/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jackyang.springbootmall.service.impl;
 
+import com.jackyang.springbootmall.constant.ProductCategory;
 import com.jackyang.springbootmall.dao.ProductDao;
 import com.jackyang.springbootmall.dto.ProductRequest;
 import com.jackyang.springbootmall.model.Product;
@@ -16,8 +17,8 @@ public class ProductServiceImpl implements ProductService {
     private ProductDao productDao;
 
     @Override
-    public List<Product> getProducts() {
-        return productDao.getProducts();
+    public List<Product> getProducts(ProductCategory category, String search) {
+        return productDao.getProducts(category, search);
     }
 
     @Override
